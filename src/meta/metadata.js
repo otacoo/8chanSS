4e383612b51b4f7b0812462986f0054c0ec98910
otakudude <208893667+otacoo@users.noreply.github.com>
2025-05-11 23:11:14 +0100

    Update metadata.js

--- src/meta/metadata.js
+++ src/meta/metadata.js
@@ -31,6 +31,12 @@
 // @grant        GM.setValue
 // @grant        GM.deleteValue
 // @grant        GM.listValues
+// @grant        GM.addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_addStyle
 // @run-at       document-start
 // @updateURL    <%= meta.downloads %><%= meta.files.metajs %>
 // @downloadURL  <%= meta.downloads %><%= meta.files.userjs %>
