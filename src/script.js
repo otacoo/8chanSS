063ba14a0e5ab3f27adc919f6fecdec15c2125ea
otacoo <208893667+otacoo@users.noreply.github.com>
2025-05-14 21:50:08 +0100

    Adjust 8chan thumbnailer threshold (220px?)

--- src/script.js
+++ src/script.js
@@ -1004,7 +1004,7 @@ onReady(async function () {
             const parentA = thumbNode.closest("a.linkThumb, a.imgLink");
             const fileWidth = parentA ? parseInt(parentA.getAttribute("data-filewidth"), 10) : null;
             const fileHeight = parentA ? parseInt(parentA.getAttribute("data-fileheight"), 10) : null;
-            const isSmallImage = (fileWidth && fileWidth < 250) || (fileHeight && fileHeight < 250);
+            const isSmallImage = (fileWidth && fileWidth < 220) || (fileHeight && fileHeight < 220);
 
             // Special case: small PNG, no t_, no extension: leave src alone
             if (
