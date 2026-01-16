a9f666f06b04930c98ebd7396d50be574c64ec15
otacoo <208893667+otacoo@users.noreply.github.com>
2026-01-16 19:09:57 +0000

    Update metadata.js

--- src/meta/metadata.js
+++ src/meta/metadata.js
@@ -32,6 +32,11 @@
 // @grant        GM.xmlHttpRequest
 // @connect      youtube.com
 // @connect      i.ytimg.com
+// @connect      twitch.tv
+// @connect      static-cdn.jtvnw.net
+// @connect      clips-media-assets2.twitch.tv
+// @connect      publish.twitter.com
+// @connect      embed.bsky.app
 // @run-at       document-start
 // @updateURL    <%= meta.downloads %><%= meta.files.metajs %>
 // @downloadURL  <%= meta.downloads %><%= meta.files.userjs %>
