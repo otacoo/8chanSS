eb214890ebc77292205bbc25abccc248bbdd5a61
otacoo <208893667+otacoo@users.noreply.github.com>
2026-04-27 13:44:51 +0100

    Update 8chanSS.user.js

--- builds/8chanSS.user.js
+++ builds/8chanSS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         8chanSS
-// @version      1.60.4
+// @version      1.60.3
 // @namespace    8chanss
 // @description  A userscript to add functionality to 8chan.
 // @author       otakudude
@@ -124,7 +124,7 @@ onReady(async function () {
     const divPosts = document.querySelector('.divPosts');
     const opHeadTitle = document.querySelector('.opHead.title');
     const catalogDiv = document.querySelector('.catalogDiv');
-    const VERSION = "1.60.4";
+    const VERSION = "1.60.3";
     const scriptSettings = {
         site: {
             _siteTWTitle: { type: "title", label: ":: Thread Watcher" },
